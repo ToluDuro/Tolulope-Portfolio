bd1a833e602993f6227fb98f8d1f671ddafb2bfd
ToluDuro <toluduro77@gmail.com>
2025-03-19 11:25:40 -0600

    Update JS code

--- style.js
+++ style.js
@@ -1,3 +1,30 @@
 document.querySelector(".menu-toggle").addEventListener("click", function() {
     document.querySelector(".nav-links").classList.toggle("show");
 });
+
+const emailTemplateId="template_rnycn5n";
+const serviceId="service_qt1c6bw";
+
+const nameInput = document.getElementById("name"); 
+const emailInput = document.getElementById("email");
+const messageInput = document.getElementById("message");
+const submitForm = (event) => {
+    event.preventDefault();
+    const nameValue = nameInput.value;
+    const emailValue = emailInput.value;
+    const messageValue = messageInput.value;
+    const templateParams = {
+        name: nameValue,
+        email: emailValue,
+        message: messageValue,
+    };
+    emailjs.send(serviceId, emailTemplateId, templateParams)
+    .then((response) => {
+        console.log("Email sent successfully!", response.status, response.text);
+        alert("Your message has been sent successfully!");
+    })
+    .catch((error) => {
+        console.error("Failed to send email:", error);
+        alert("Failed to send your message. Please try again later.");
+    });
+};
